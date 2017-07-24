8236dd6a3601d4f50da8b936df8131488ad7316b
myunusov <myunusov@luxoft.com>
2017-07-24 16:31:15 +0600

    DEV: Add Yaml and Json properties support #10 #63

--- maxur-mserv-core/src/main/kotlin/org/maxur/mserv/core/service/properties/PropertiesServiceJacksonImpl.kt
+++ maxur-mserv-core/src/main/kotlin/org/maxur/mserv/core/service/properties/PropertiesServiceJacksonImpl.kt
@@ -4,28 +4,33 @@ import com.fasterxml.jackson.core.JsonFactory
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import java.io.File
+import java.io.InputStream
 import java.net.URI
 import java.nio.file.Paths
 
 class PropertiesServiceJacksonImpl(override val source: PropertiesSource, factory: JsonFactory) : PropertiesService {
+
     val mapper = ObjectMapper(factory)
+
     var root: JsonNode = jsonNode(source.uri!!)!!
 
     private fun jsonNode(uri: URI): JsonNode? {
-        val result = when {
-            uri.scheme == null -> mapper.readTree(File(uri.toString()))
-            uri.scheme == "file" -> mapper.readTree(Paths.get(uri).toFile())
-            uri.scheme == "classpath" -> {
-                val name = "/" + uri.toString().substring("classpath".length + 1).trimStart('/')
-                val io = this::class.java.getResourceAsStream(name) ?:
-                        throw IllegalArgumentException("""Resource '$name' is not found""")
-                mapper.readTree(io)
-            }
-            else -> throw IllegalArgumentException(
-                    """Unsupported schema '${uri.scheme}' to properties source. Must be one of [file, classpath]"""
-            )
-        }
-        return result?.path(source.rootKey)
+        return rootNode(uri)?.path(source.rootKey)
+    }
+
+    private fun rootNode(uri: URI): JsonNode? = when {
+        uri.scheme == null -> mapper.readTree(File(uri.toString()))
+        uri.scheme == "file" -> mapper.readTree(Paths.get(uri).toFile())
+        uri.scheme == "classpath" -> mapper.readTree(inputStreamByResource(uri))
+        else -> throw IllegalArgumentException(
+                """Unsupported schema '${uri.scheme}' to properties source. Must be one of [file, classpath]"""
+        )
+    }
+
+    private fun inputStreamByResource(uri: URI): InputStream {
+        val name = "/" + uri.toString().substring("classpath".length + 1).trimStart('/')
+        return this::class.java.getResourceAsStream(name) ?:
+                throw IllegalArgumentException("""Resource '$name' is not found""")
     }
 
     override fun asString(key: String): String? = root.path(key).asText()
