058be51c7a5d66e37d593484c25733396cf262cc
myunusov <myunusov@luxoft.com>
2017-08-22 18:22:00 +0600

    DEV: Add Tests on FaviconResource

---
+++ maxur-mserv-core/src/test/kotlin/org/maxur/mserv/core/rest/FaviconResourceIT.kt
@@ -0,0 +1,37 @@
+package org.maxur.mserv.core.rest
+
+import org.assertj.core.api.Assertions
+import org.glassfish.hk2.utilities.binding.AbstractBinder
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+import java.io.IOException
+import kotlin.reflect.KClass
+
+@RunWith(MockitoJUnitRunner::class)
+class FaviconResourceIT : AbstractResourceAT() {
+
+    override fun configurator() = { _: AbstractBinder -> }
+
+    override fun resourceClass(): KClass<out Any> = FaviconResource::class
+
+    @Test
+    @Throws(IOException::class)
+    fun testFaviconGet() {
+        val baseTarget = target("/favicon.ico")
+        val array = baseTarget.request()
+                .accept("image/x-icon")
+                .get(ByteArray::class.java)
+        Assertions.assertThat(array).isNotEmpty()
+    }
+
+    @Test
+    @Throws(IOException::class)
+    fun testInvalidFaviconGet() {
+        val baseTarget = target("/invalid.ico")
+        val response= baseTarget.request()
+                .accept("image/x-icon")
+                .get()
+        Assertions.assertThat(response.status).isEqualTo(404)
+    }
+}
