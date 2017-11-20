44e6c9387b07fc3cfd4a376dfc575f8f544cd3cd
Maxim Yunusov <maximyunusov@maxur.org>
2017-11-20 21:26:35 +0600

    FIX

--- maxur-mserv-core/src/main/kotlin/org/maxur/mserv/core/rest/RunningCommandResource.kt
+++ maxur-mserv-core/src/main/kotlin/org/maxur/mserv/core/rest/RunningCommandResource.kt
@@ -12,6 +12,8 @@ import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
 import io.swagger.annotations.ApiResponse
 import io.swagger.annotations.ApiResponses
+import io.swagger.annotations.Example
+import io.swagger.annotations.ExampleProperty
 import org.hibernate.validator.constraints.NotBlank
 import org.maxur.mserv.core.MicroService
 import java.net.URI
@@ -34,68 +36,90 @@ import javax.ws.rs.core.MediaType
  */
 @Path("/service/command")
 @Api(value = "/service/command", description = "Endpoint for Service specific operations")
-class RunningCommandResource @Inject constructor(val service: MicroService) {
+class RunningCommandResource @Inject constructor(
+    /** the Microservice */
+    val service: MicroService
+) {
 
+    /**  Returns all running commands */
     @GET
     @Produces("application/hal+json")
     @ApiOperation(value = "Represent this service running commands",
-            response = ServiceView::class, produces = "application/hal+json")
+        response = ServiceView::class, produces = "application/hal+json")
     @ApiResponses(value = *arrayOf(
-            ApiResponse(code = 200, message = "Successful operation"),
-            ApiResponse(code = 500, message = "Internal server error")
+        ApiResponse(code = 200, message = "Successful operation"),
+        ApiResponse(code = 500, message = "Internal server error")
     )
     )
-    fun service(): RunningCommandsView = RunningCommandsView()
+    fun commands(): RunningCommandsView = RunningCommandsView()
 
+    /** Add new command to queue */
     @POST()
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     @ApiOperation(
-            value = "Insert new command",
-            notes = "Commands for stop or restart service"
+        value = "Insert new command",
+        notes = "Commands for stop or restart service"
     )
     @ApiResponses(value = *arrayOf(
-            ApiResponse(code = 204, message = "Successful operation"),
-            ApiResponse(code = 400, message = "On invalid command"),
-            ApiResponse(code = 500, message = "Internal server error")
+        ApiResponse(code = 204, message = "Successful operation"),
+        ApiResponse(code = 400, message = "On invalid command"),
+        ApiResponse(code = 500, message = "Internal server error")
     )
     )
-    fun state(
-            @ApiParam(name = "command", value = "New service command", required = true)
-            @Valid command: ServiceCommand
+    fun command(
+        @ApiParam(
+            name = "command",
+            type = "object",
+            value = "New service command",
+            required = true,
+            examples = Example(value = ExampleProperty(
+                mediaType = "application/json", value = "{ \"type\": \"restart\" }"
+            ))
+        )
+        @Valid command: ServiceCommand
     ) = when (command.type.toUpperCase()) {
         "STOP" -> service.deferredStop()
         "RESTART" -> service.deferredRestart()
         else -> throw IllegalArgumentException("Command '$command' unknown")
     }
 
+    /** The service command */
     @ApiModel(
-            value = "TenantConfiguration", description = "This class represents the service command"
+        value = "ServiceCommand",
+        description = "This class represents the service command"
     )
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     data class ServiceCommand(
-            @ApiModelProperty(
-                    name = "type",
-                    value = "type of command",
-                    required = true,
-                    allowableValues = "stop, restart"
-            )
-            @NotBlank
-            @Pattern(regexp = "^(stop|restart)$")
-            @JsonProperty("type")
-            val type: String
+        /** The command type */
+        @ApiModelProperty(
+            dataType = "string",
+            name = "type",
+            value = "type of the command",
+            notes = "Type of the command",
+            required = true,
+            allowableValues = "stop, restart",
+            example = "restart"
+        )
+        @NotBlank
+        @Pattern(regexp = "^(stop|restart)$")
+        @JsonProperty("type")
+        val type: String
     )
 }
 
+/** Running command endpoint HAL view */
 @Suppress("unused")
 @Resource
 @ApiModel(value = "Service Running Commands View", description = "Service Running Commands Model")
-class RunningCommandsView() {
+class RunningCommandsView {
 
+    /** List of commands in execution queue */
     @ApiModelProperty(value = "Service Running Commands")
     val commands: List<String> = emptyList()
 
+    /** This endpoint */
     @Link
-    var self: HALLink = HALLink.Builder(URI("service/command")).build()
+    var self: HALLink = HALLink.Builder(URI("command")).build()
 
 }
